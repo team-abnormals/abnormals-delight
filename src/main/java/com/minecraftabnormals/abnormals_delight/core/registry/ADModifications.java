8f8708bbfb71586932a7f77506dfd612f4c6ad02
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2021-01-26 09:15:17 -0600

    fixed borked stuff

--- src/main/java/com/minecraftabnormals/abnormals_delight/core/registry/ADModifications.java
+++ src/main/java/com/minecraftabnormals/abnormals_delight/core/registry/ADModifications.java
@@ -1,35 +1,42 @@
 package com.minecraftabnormals.abnormals_delight.core.registry;
 
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Lists;
-import com.minecraftabnormals.abnormals_core.core.util.registry.BlockSubRegistryHelper;
-import com.minecraftabnormals.abnormals_delight.core.AbnormalsDelight;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.registries.ForgeRegistries;
 import vectorwing.farmersdelight.FarmersDelight;
-import vectorwing.farmersdelight.blocks.*;
+import vectorwing.farmersdelight.blocks.PantryBlock;
+import vectorwing.farmersdelight.blocks.WildPatchBlock;
+import vectorwing.farmersdelight.blocks.WildRiceBlock;
 import vectorwing.farmersdelight.items.KnifeItem;
-import vectorwing.farmersdelight.registry.ModBlocks;
 import vectorwing.farmersdelight.registry.ModItems;
-import vectorwing.farmersdelight.utils.tags.ModTags;
 
-import java.util.List;
 import java.util.Set;
 
 public class ADModifications {
+	private static final Set<Item> MATERIALS = ImmutableSet.of(ModItems.STRAW.get(), ModItems.CANVAS.get(), ModItems.TREE_BARK.get(), ModItems.CABBAGE_SEEDS.get(), ModItems.TOMATO_SEEDS.get(), ModItems.RICE_PANICLE.get(), ModItems.RICE.get());
+	private static final Set<Item> DECORATIONS = ImmutableSet.of(ModItems.STOVE.get(), ModItems.COOKING_POT.get(), ModItems.CUTTING_BOARD.get(), ModItems.BEETROOT_CRATE.get(), ModItems.CARROT_CRATE.get(), ModItems.POTATO_CRATE.get(), ModItems.ONION_CRATE.get(), ModItems.CABBAGE_CRATE.get(), ModItems.RICE_BAG.get(), ModItems.TOMATO_CRATE.get(), ModItems.RICE_BALE.get(), ModItems.STRAW_BALE.get(), ModItems.RICH_SOIL_FARMLAND.get(), ModItems.FULL_TATAMI_MAT.get(), ModItems.HALF_TATAMI_MAT.get(), ModItems.BROWN_MUSHROOM_COLONY.get(), ModItems.RED_MUSHROOM_COLONY.get(), ModItems.BASKET.get(), ModItems.SAFETY_NET.get(), ModItems.ROPE.get());
+	private static final Set<Item> BUILDING_BLOCKS = ImmutableSet.of(ModItems.ORGANIC_COMPOST.get(), ModItems.RICH_SOIL.get(), ModItems.TATAMI.get());
 	private static final Set<Item> FOOD = ImmutableSet.of(ModItems.HORSE_FEED.get(), ModItems.ROAST_CHICKEN_BLOCK.get(), ModItems.STUFFED_PUMPKIN_BLOCK.get(), ModItems.HONEY_GLAZED_HAM_BLOCK.get(), ModItems.APPLE_PIE.get(), ModItems.SWEET_BERRY_CHEESECAKE.get(), ModItems.CHOCOLATE_PIE.get(), ModItems.HOT_COCOA.get(), ModItems.MILK_BOTTLE.get());
 
 	public static void replaceItemGroups() {
-		for(Item item : ForgeRegistries.ITEMS.getValues()) {
+		for (Item item : ForgeRegistries.ITEMS.getValues()) {
 			if (item.getRegistryName().getNamespace().equals(FarmersDelight.MODID)) {
 				if (item.isFood()) setItemGroup(item, ItemGroup.FOOD);
+				else if (item instanceof KnifeItem) setItemGroup(item, ItemGroup.COMBAT);
+				else if (MATERIALS.contains(item)) setItemGroup(item, ItemGroup.MATERIALS);
 				else if (DECORATIONS.contains(item)) setItemGroup(item, ItemGroup.DECORATIONS);
+				else if (BUILDING_BLOCKS.contains(item)) setItemGroup(item, ItemGroup.BUILDING_BLOCKS);
 				else if (FOOD.contains(item)) setItemGroup(item, ItemGroup.FOOD);
+				else if (item instanceof BlockItem) {
+					Block block = ((BlockItem) item).getBlock();
+					if (block instanceof PantryBlock || block instanceof WildPatchBlock || block instanceof WildRiceBlock) {
+						setItemGroup(item, ItemGroup.DECORATIONS);
+					}
+				}
 			}
 		}
 	}
