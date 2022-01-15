30baba69488ae0b47a7911e553e573f6d938a4fc
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2022-01-15 10:24:26 -0600

    Fix Neapolitan Cakes not mining faster with knives

--- src/main/java/com/teamabnormals/abnormals_delight/core/data/server/tags/ADBlockTagsProvider.java
+++ src/main/java/com/teamabnormals/abnormals_delight/core/data/server/tags/ADBlockTagsProvider.java
@@ -11,8 +11,8 @@ import vectorwing.farmersdelight.common.tag.ModTags;
 
 public class ADBlockTagsProvider extends BlockTagsProvider {
 
-	public ADBlockTagsProvider(DataGenerator generator, ExistingFileHelper existingFileHelper) {
-		super(generator, AbnormalsDelight.MOD_ID, existingFileHelper);
+	public ADBlockTagsProvider(DataGenerator generator, ExistingFileHelper fileHelper) {
+		super(generator, AbnormalsDelight.MOD_ID, fileHelper);
 	}
 
 	@Override
@@ -20,5 +20,6 @@ public class ADBlockTagsProvider extends BlockTagsProvider {
 		this.tag(BlockTags.MINEABLE_WITH_AXE).add(ADBlocks.ROSEWOOD_CABINET.get(), ADBlocks.MORADO_CABINET.get(), ADBlocks.YUCCA_CABINET.get(), ADBlocks.KOUSA_CABINET.get(), ADBlocks.ASPEN_CABINET.get(), ADBlocks.GRIMWOOD_CABINET.get(), ADBlocks.MAPLE_CABINET.get(), ADBlocks.AZALEA_CABINET.get(), ADBlocks.POISE_CABINET.get(), ADBlocks.WILLOW_CABINET.get(), ADBlocks.CHERRY_CABINET.get(), ADBlocks.WISTERIA_CABINET.get(), ADBlocks.DRIFTWOOD_CABINET.get(), ADBlocks.RIVER_CABINET.get());
 		this.tag(ModTags.HEAT_SOURCES).addOptional(ADConstants.GLOWING_OBSIDIAN);
 		this.tag(ModTags.TRAY_HEAT_SOURCES).addOptionalTag(ADConstants.BRAZIERS);
+		this.tag(ModTags.MINEABLE_WITH_KNIFE).addOptional(ADConstants.VANILLA_CAKE).addOptional(ADConstants.CHOCOLATE_CAKE).addOptional(ADConstants.STRAWBERRY_CAKE).addOptional(ADConstants.BANANA_CAKE).addOptional(ADConstants.MINT_CAKE).addOptional(ADConstants.ADZUKI_CAKE);
 	}
 }
