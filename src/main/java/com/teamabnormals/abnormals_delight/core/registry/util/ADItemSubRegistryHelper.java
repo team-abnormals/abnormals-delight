8ea2d09bb95184fd6226a031811b61441658674d
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-03-27 11:38:37 -0500

    Remove unused class

--- src/main/java/com/teamabnormals/abnormals_delight/core/registry/util/ADItemSubRegistryHelper.java
+++ src/main/java/com/teamabnormals/abnormals_delight/core/registry/util/ADItemSubRegistryHelper.java
@@ -1,7 +1,6 @@
 package com.teamabnormals.abnormals_delight.core.registry.util;
 
 import com.teamabnormals.abnormals_delight.common.item.ContainerConsumableItem;
-import com.teamabnormals.abnormals_delight.common.item.UseTimeItem;
 import com.teamabnormals.blueprint.core.util.registry.ItemSubRegistryHelper;
 import com.teamabnormals.blueprint.core.util.registry.RegistryHelper;
 import net.minecraft.resources.ResourceLocation;
@@ -18,10 +17,6 @@ public class ADItemSubRegistryHelper extends ItemSubRegistryHelper {
 
 	public RegistryObject<Item> createCompatConsumableItem(String name, Item.Properties properties, CreativeModeTab group, String... modIds) {
 		return this.deferredRegister.register(name, () -> new ConsumableItem(properties.tab(areModsLoaded(modIds) ? group : null)));
-	}
-
-	public RegistryObject<Item> createCompatUseTimeItem(String name, Item.Properties properties, CreativeModeTab group, String... modIds) {
-		return this.deferredRegister.register(name, () -> new UseTimeItem(properties.tab(areModsLoaded(modIds) ? group : null)));
 	}
 
 	public RegistryObject<Item> createCompatContainerConsumableItem(String name, ResourceLocation item, Item.Properties properties, CreativeModeTab group, String... modIds) {
