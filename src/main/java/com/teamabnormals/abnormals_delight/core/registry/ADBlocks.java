e75171cf407ad920eb9b03449c4eba6e1b652b40
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2025-09-06 00:27:25 -0500

    Revert "furnace fuels"
    This reverts commit 5ed1f149fa526ebd7d99565baf7cabfa9201dbcb.

--- src/main/java/com/teamabnormals/abnormals_delight/core/registry/ADBlocks.java
+++ src/main/java/com/teamabnormals/abnormals_delight/core/registry/ADBlocks.java
@@ -10,20 +10,20 @@ import vectorwing.farmersdelight.common.block.CabinetBlock;
 public class ADBlocks {
 	public static final BlockSubRegistryHelper BLOCKS = AbnormalsDelight.REGISTRY_HELPER.getBlockSubHelper();
 
-	public static final DeferredBlock<Block> ROSEWOOD_CABINET = BLOCKS.createBlock("rosewood_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)));
-	public static final DeferredBlock<Block> MORADO_CABINET = BLOCKS.createBlock("morado_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)));
-	public static final DeferredBlock<Block> YUCCA_CABINET = BLOCKS.createBlock("yucca_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)));
-	public static final DeferredBlock<Block> KOUSA_CABINET = BLOCKS.createBlock("kousa_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)));
-	public static final DeferredBlock<Block> ASPEN_CABINET = BLOCKS.createBlock("aspen_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)));
-	public static final DeferredBlock<Block> LAUREL_CABINET = BLOCKS.createBlock("laurel_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)));
-	public static final DeferredBlock<Block> GRIMWOOD_CABINET = BLOCKS.createBlock("grimwood_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)));
-	public static final DeferredBlock<Block> MAPLE_CABINET = BLOCKS.createBlock("maple_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)));
-	public static final DeferredBlock<Block> AZALEA_CABINET = BLOCKS.createBlock("azalea_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)));
-	public static final DeferredBlock<Block> POISE_CABINET = BLOCKS.createBlock("poise_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)));
-	public static final DeferredBlock<Block> WILLOW_CABINET = BLOCKS.createBlock("willow_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)));
-	public static final DeferredBlock<Block> WISTERIA_CABINET = BLOCKS.createBlock("wisteria_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)));
-	public static final DeferredBlock<Block> PLUM_CABINET = BLOCKS.createBlock("plum_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)));
-	public static final DeferredBlock<Block> PINE_CABINET = BLOCKS.createBlock("pine_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)));
-	public static final DeferredBlock<Block> DRIFTWOOD_CABINET = BLOCKS.createBlock("driftwood_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)));
-	public static final DeferredBlock<Block> RIVER_CABINET = BLOCKS.createBlock("river_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)));
+	public static final DeferredBlock<Block> ROSEWOOD_CABINET = BLOCKS.createFuelBlock("rosewood_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)), 300);
+	public static final DeferredBlock<Block> MORADO_CABINET = BLOCKS.createFuelBlock("morado_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)), 300);
+	public static final DeferredBlock<Block> YUCCA_CABINET = BLOCKS.createFuelBlock("yucca_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)), 300);
+	public static final DeferredBlock<Block> KOUSA_CABINET = BLOCKS.createFuelBlock("kousa_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)), 300);
+	public static final DeferredBlock<Block> ASPEN_CABINET = BLOCKS.createFuelBlock("aspen_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)), 300);
+	public static final DeferredBlock<Block> LAUREL_CABINET = BLOCKS.createFuelBlock("laurel_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)), 300);
+	public static final DeferredBlock<Block> GRIMWOOD_CABINET = BLOCKS.createFuelBlock("grimwood_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)), 300);
+	public static final DeferredBlock<Block> MAPLE_CABINET = BLOCKS.createFuelBlock("maple_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)), 300);
+	public static final DeferredBlock<Block> AZALEA_CABINET = BLOCKS.createFuelBlock("azalea_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)), 300);
+	public static final DeferredBlock<Block> POISE_CABINET = BLOCKS.createFuelBlock("poise_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)), 300);
+	public static final DeferredBlock<Block> WILLOW_CABINET = BLOCKS.createFuelBlock("willow_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)), 300);
+	public static final DeferredBlock<Block> WISTERIA_CABINET = BLOCKS.createFuelBlock("wisteria_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)), 300);
+	public static final DeferredBlock<Block> PLUM_CABINET = BLOCKS.createFuelBlock("plum_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)), 300);
+	public static final DeferredBlock<Block> PINE_CABINET = BLOCKS.createFuelBlock("pine_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)), 300);
+	public static final DeferredBlock<Block> DRIFTWOOD_CABINET = BLOCKS.createFuelBlock("driftwood_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)), 300);
+	public static final DeferredBlock<Block> RIVER_CABINET = BLOCKS.createFuelBlock("river_cabinet", () -> new CabinetBlock(Block.Properties.ofFullCopy(Blocks.BARREL)), 300);
 }
